cb054dba6d960827778369a249f390d27704c732
muzammil khanm <khanmuzammil549@gmail.com>
2025-01-16 22:31:33 +0530

    add category to Event schema

--- models/Event.js
+++ models/Event.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
   title: {type: String, required: true},
+  category: {type: String, required: true},
   venue: {type: String, required: true},
   summary: {type: String, required: true},
   date: {type: Date, required: true},
