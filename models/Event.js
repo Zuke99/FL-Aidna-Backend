df3433b9330d9a02897f59a2280b1ebeb86cfc59
muzammil khanm <khanmuzammil549@gmail.com>
2025-01-18 23:23:11 +0530

    update schema for event and user

--- models/Event.js
+++ models/Event.js
@@ -1,20 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const EventSchema = new mongoose.Schema({
-  title: {type: String, required: true},
-  category: {type: String, required: true},
-  venue: {type: String, required: true},
-  summary: {type: String, required: true},
-  date: {type: Date, required: true},
-  image: {type: String, required: true, default: 'https://res.cloudinary.com/dk5b3f3zr/image/upload/v1628585794/avatars/avatar-1_ukzj6s.png'},
-  creator: {type: mongoose.Types.ObjectId, required: true, ref: 'User'},
-  price: {type: Number, required: true},
-  totalTickets: {type: Number, required: true},
-  availableTickets: {type: Number, required: true},
-  isCancelled: {type: Boolean, default: false},
-  isSoldOut: {type: Boolean, default: false},
-  isFeatured: {type: Boolean, default: false},
-  aboutEvent: {type: String},
-}, { timestamps: true })
+const EventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    venue: { type: String, required: true },
+    summary: { type: String, required: true },
+    date: { type: Date, required: true },
+    image: {
+      type: String,
+      required: true,
+      default:
+        "https://res.cloudinary.com/dk5b3f3zr/image/upload/v1628585794/avatars/avatar-1_ukzj6s.png",
+    },
+    creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    price: { type: Number, required: true },
+    totalTickets: { type: Number, required: true },
+    availableTickets: { type: Number, required: true },
+    isCancelled: { type: Boolean, default: false },
+    isSoldOut: { type: Boolean, default: false },
+    isFeatured: { type: Boolean, default: false },
+    aboutEvent: { type: String },
+    purchasers: [
+      {
+        userId: { type: mongoose.Types.ObjectId, ref: "User" },
+        ticketsPurchased: { type: Number, required: true },
+        totalAmount: { type: Number, required: true },
+        purchasedAt: { type: Date, default: Date.now },
+        method: { type: String },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Event', EventSchema);
+module.exports = mongoose.model("Event", EventSchema);
--- models/User.js
+++ models/User.js
@@ -1,15 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
-  userName: { type: String, required: true },
-  role: { type: String, required: true, enum: ['admin', 'moderator', 'user'], default: 'user' },
-  creator: { type: Boolean, default: false },
-  totalEvents: { type: Number, default: 0 },
-  totalFollowers: { type: Number, default: 0 },
-  isVerified: { type: Boolean, default: false },
-  image: { type: String, default: 'https://res.cloudinary.com/dk5b3f3zr/image/upload/v1628585794/avatars/avatar-1_ukzj6s.png' },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    userName: { type: String, required: true },
+    role: {
+      type: String,
+      required: true,
+      enum: ["admin", "moderator", "user"],
+      default: "user",
+    },
+    creator: { type: Boolean, default: false },
+    isVerified: { type: Boolean, default: false },
+    image: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dk5b3f3zr/image/upload/v1628585794/avatars/avatar-1_ukzj6s.png",
+    },
+    events: [
+      {
+        eventId: { type: mongoose.Types.ObjectId, ref: "Event" },
+        ticketsPurchased: { type: Number, required: true },
+        totalAmount: { type: Number, required: true },
+        purchasedAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
