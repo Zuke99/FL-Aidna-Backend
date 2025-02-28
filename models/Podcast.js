8f0175a67494eb132c3160842b5e924438aa7123
Dheeraj <dheeruzuke99@gmail.com>
2025-03-01 04:06:39 +0530

    functionality of WebinarandPodcast

--- models/Podcast.js
+++ models/Podcast.js
@@ -4,7 +4,7 @@ const podcastSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   speaker: { type: String, required: true },
-  uploadedBy: { type: String, required: true },
+  uploadedBy: { type: String, required: true , default: 'Admin'},
   date: { type: String, required: true },
   category: { type: String, required: true },
   label: { type: String, required: true },
