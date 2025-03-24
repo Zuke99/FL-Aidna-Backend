61d35c6ea9fe9a9d31b3c3edeb160b5d8e80f9a9
muzammil khan <khanmuzammil549@gmail.com>
2025-03-24 15:31:08 +0530

    update Event and add coupons

---
+++ controllers/couponController.js
@@ -0,0 +1,71 @@
+const couponService = require('../services/couponService');
+
+const getCoupon = async (req, res) => {
+  try {
+    const { code } = req.params;
+    const coupon = await couponService.getCouponByCode(code);
+    if (!coupon) return res.status(404).json({ message: 'Coupon not found or expired' });
+    res.json(coupon);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const createCoupon = async (req, res) => {
+  try {
+    const coupon = await couponService.createCoupon(req.body);
+    res.status(201).json(coupon);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// const redeemCoupon = async (req, res) => {
+//   try {
+//     const { code } = req.params;
+//     const userId = req.user.id; // Assuming user is authenticated
+//     const coupon = await couponService.redeemCoupon(userId, code);
+//     res.json({ message: 'Coupon redeemed successfully', coupon });
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
+
+const redeemCoupon = async (req, res) => {
+  try {
+    const { code } = req.params;
+
+    const coupon = await couponService.redeemCoupon(code);
+
+    res.json({ message: 'Coupon redeemed successfully', coupon });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getAllCoupons = async (req, res) => {
+  try {
+    const coupons = await couponService.getAllCoupons();
+    res.json(coupons);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteCoupon = async (req, res) => {
+  try {
+    const { code } = req.params;
+    await couponService.deleteCoupon(code);
+    res.json({ message: 'Coupon deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getCoupon,
+  createCoupon,
+  redeemCoupon,
+  getAllCoupons,
+  deleteCoupon
+};
---
+++ models/Coupon.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const couponSchema = new mongoose.Schema({
+  couponCode: { type: String, required: true, unique: true, trim: true, uppercase: true },
+  discountAmount: { type: Number, required: true, min: 0 },
+  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: false },
+  limit: { type: Number, default: 1 },
+  usersRedeemed: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      count: { type: Number, default: 1 },
+    },
+  ],
+  isActive: { type: Boolean, default: true },
+});
+
+const Coupon = mongoose.model("Coupon", couponSchema)
+
+module.exports = Coupon;
