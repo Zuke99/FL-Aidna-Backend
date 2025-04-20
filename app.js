cf3f9d70343a1922ba4dff7b99a7b42ffecacb8c
muzammil khan <khanmuzammil549@gmail.com>
2025-04-21 00:44:46 +0530

    storage v2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors'); // Import the cors package
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 const authRoutes = require('./routes/auth');
@@ -60,6 +61,16 @@ app.use('/api/expos-banner', exposBannerRoutes)
 app.use('/api/videos', videosRoutes)
 app.use('/api/mail', mailRoutes)
 app.use('/api/header', HeaderLogoRoutes)
+const uploadDir = path.join(__dirname, '../uploads');
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true }); // recursive handles nested paths
+  console.log('✅ uploads folder created automatically');
+}
+
+// Serve uploaded files statically
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
 
 app.use(express.static(path.join(__dirname,'dist')))
 app.get('*', (req, res) => {
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,25 @@
+// backend/routes/uploadRoutes.js
 const express = require('express');
-const { uploadFile, uploadMiddleware } = require('../controllers/uploadController');
-
+const { upload } = require('../controllers/uploadController');
 const router = express.Router();
 
-router.post('/', uploadMiddleware, uploadFile);
+// Route to handle single file upload
+router.post('/', upload.single('media'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ success: false, message: 'No file uploaded' });
+  }
+
+  // Generate the file URL to be sent back to the client
+  // FOr production
+  // const fileUrl = `https://media2ai.com/uploads/${req.file.filename}`;
+  const fileUrl = `https://staging-media2ai.tekit.in/uploads/${req.file.filename}`;
+  
+  
+  res.status(200).json({
+    success: true,
+    message: 'File uploaded successfully',
+    signed_url: fileUrl,
+  });
+});
 
 module.exports = router;
